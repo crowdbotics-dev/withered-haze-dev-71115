6795356d92b23ad320849f469b8928f519ead2b6
Crowdbotics <engineering@crowdbotics.com>
2023-03-22 00:17:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-haze-dev-71115",
+  url: "https://withered-haze-dev-71115.botics.co",
+  api: "https://withered-haze-dev-71115.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
